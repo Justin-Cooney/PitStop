7cac654979afa94d9d36547a85f83dc40ce28bcc
Justin-Cooney <justin.d.cooney@gmail.com>
2020-02-01 15:51:27 -0600

    FIRE

---
+++ Assets/Scripts/Lighting/FlashingLight.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashingLight : MonoBehaviour
+{
+    private Light _light;
+    private bool _increasing;
+
+    void Start()
+    {
+        _light = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_increasing)
+            _light.intensity += 1 * Time.deltaTime;
+        else
+            _light.intensity -= 1 * Time.deltaTime;
+
+        if (_light.intensity <= 0)
+            _increasing = true;
+        if (_light.intensity >= 1)
+            _increasing = false;
+    }
+}
