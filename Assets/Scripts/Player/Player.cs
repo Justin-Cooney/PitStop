7051eb188d53d5558c292847a465181c154f7206
Justin-Cooney <justin.d.cooney@gmail.com>
2020-01-31 22:33:25 -0600

    Movement

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,22 +5,46 @@ using static UnityEngine.Experimental.Input.InputAction;
 
 public class Player : MonoBehaviour
 {
-	PlayerInput _playerInput;
+	private PlayerInput _playerInput;
+	private CharacterController _characterController;
+	private float _speed = 5;
+
+	private bool _moveUp = false;
 
 	[Inject]
 	public void Initialize(PlayerInput playerInput)
 	{
 		_playerInput = playerInput;
+		_characterController = this.GetComponent<CharacterController>();
 		_playerInput.PlayerControls.Enable();
 		_playerInput.PlayerControls.MoveUp.performed += OnMoveUp;
+		_playerInput.PlayerControls.MoveDown.performed += OnMoveDown;
+		_playerInput.PlayerControls.MoveLeft.performed += OnMoveLeft;
+		_playerInput.PlayerControls.MoveRight.performed += OnMoveRight;
 	}
 
 	public void Update()
 	{
+		
 	}
 
 	private void OnMoveUp(CallbackContext context)
 	{
-		Debug.Log("Up is pressed yo!");
+		_characterController.Move(Vector3.forward * _speed * Time.deltaTime);
+	}
+
+	private void OnMoveDown(CallbackContext context)
+	{
+		_characterController.Move(Vector3.back * _speed * Time.deltaTime);
+	}
+
+	private void OnMoveLeft(CallbackContext context)
+	{
+		_characterController.Move(Vector3.left * _speed * Time.deltaTime);
+	}
+
+	private void OnMoveRight(CallbackContext context)
+	{
+		_characterController.Move(Vector3.right * _speed * Time.deltaTime);
 	}
 }
--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -30,6 +30,27 @@ namespace Assets.Scripts.Player
                 m_PlayerControls_MoveUp.performed += m_PlayerControlsMoveUpActionPerformed.Invoke;
             if (m_PlayerControlsMoveUpActionCancelled != null)
                 m_PlayerControls_MoveUp.cancelled += m_PlayerControlsMoveUpActionCancelled.Invoke;
+            m_PlayerControls_MoveDown = m_PlayerControls.GetAction("MoveDown");
+            if (m_PlayerControlsMoveDownActionStarted != null)
+                m_PlayerControls_MoveDown.started += m_PlayerControlsMoveDownActionStarted.Invoke;
+            if (m_PlayerControlsMoveDownActionPerformed != null)
+                m_PlayerControls_MoveDown.performed += m_PlayerControlsMoveDownActionPerformed.Invoke;
+            if (m_PlayerControlsMoveDownActionCancelled != null)
+                m_PlayerControls_MoveDown.cancelled += m_PlayerControlsMoveDownActionCancelled.Invoke;
+            m_PlayerControls_MoveLeft = m_PlayerControls.GetAction("MoveLeft");
+            if (m_PlayerControlsMoveLeftActionStarted != null)
+                m_PlayerControls_MoveLeft.started += m_PlayerControlsMoveLeftActionStarted.Invoke;
+            if (m_PlayerControlsMoveLeftActionPerformed != null)
+                m_PlayerControls_MoveLeft.performed += m_PlayerControlsMoveLeftActionPerformed.Invoke;
+            if (m_PlayerControlsMoveLeftActionCancelled != null)
+                m_PlayerControls_MoveLeft.cancelled += m_PlayerControlsMoveLeftActionCancelled.Invoke;
+            m_PlayerControls_MoveRight = m_PlayerControls.GetAction("MoveRight");
+            if (m_PlayerControlsMoveRightActionStarted != null)
+                m_PlayerControls_MoveRight.started += m_PlayerControlsMoveRightActionStarted.Invoke;
+            if (m_PlayerControlsMoveRightActionPerformed != null)
+                m_PlayerControls_MoveRight.performed += m_PlayerControlsMoveRightActionPerformed.Invoke;
+            if (m_PlayerControlsMoveRightActionCancelled != null)
+                m_PlayerControls_MoveRight.cancelled += m_PlayerControlsMoveRightActionCancelled.Invoke;
             m_Initialized = true;
         }
         private void Uninitialize()
@@ -46,6 +67,27 @@ namespace Assets.Scripts.Player
                 m_PlayerControls_MoveUp.performed -= m_PlayerControlsMoveUpActionPerformed.Invoke;
             if (m_PlayerControlsMoveUpActionCancelled != null)
                 m_PlayerControls_MoveUp.cancelled -= m_PlayerControlsMoveUpActionCancelled.Invoke;
+            m_PlayerControls_MoveDown = null;
+            if (m_PlayerControlsMoveDownActionStarted != null)
+                m_PlayerControls_MoveDown.started -= m_PlayerControlsMoveDownActionStarted.Invoke;
+            if (m_PlayerControlsMoveDownActionPerformed != null)
+                m_PlayerControls_MoveDown.performed -= m_PlayerControlsMoveDownActionPerformed.Invoke;
+            if (m_PlayerControlsMoveDownActionCancelled != null)
+                m_PlayerControls_MoveDown.cancelled -= m_PlayerControlsMoveDownActionCancelled.Invoke;
+            m_PlayerControls_MoveLeft = null;
+            if (m_PlayerControlsMoveLeftActionStarted != null)
+                m_PlayerControls_MoveLeft.started -= m_PlayerControlsMoveLeftActionStarted.Invoke;
+            if (m_PlayerControlsMoveLeftActionPerformed != null)
+                m_PlayerControls_MoveLeft.performed -= m_PlayerControlsMoveLeftActionPerformed.Invoke;
+            if (m_PlayerControlsMoveLeftActionCancelled != null)
+                m_PlayerControls_MoveLeft.cancelled -= m_PlayerControlsMoveLeftActionCancelled.Invoke;
+            m_PlayerControls_MoveRight = null;
+            if (m_PlayerControlsMoveRightActionStarted != null)
+                m_PlayerControls_MoveRight.started -= m_PlayerControlsMoveRightActionStarted.Invoke;
+            if (m_PlayerControlsMoveRightActionPerformed != null)
+                m_PlayerControls_MoveRight.performed -= m_PlayerControlsMoveRightActionPerformed.Invoke;
+            if (m_PlayerControlsMoveRightActionCancelled != null)
+                m_PlayerControls_MoveRight.cancelled -= m_PlayerControlsMoveRightActionCancelled.Invoke;
             m_Initialized = false;
         }
         public void SetAsset(InputActionAsset newAsset)
@@ -67,6 +109,18 @@ namespace Assets.Scripts.Player
         [SerializeField] private ActionEvent m_PlayerControlsMoveUpActionStarted;
         [SerializeField] private ActionEvent m_PlayerControlsMoveUpActionPerformed;
         [SerializeField] private ActionEvent m_PlayerControlsMoveUpActionCancelled;
+        private InputAction m_PlayerControls_MoveDown;
+        [SerializeField] private ActionEvent m_PlayerControlsMoveDownActionStarted;
+        [SerializeField] private ActionEvent m_PlayerControlsMoveDownActionPerformed;
+        [SerializeField] private ActionEvent m_PlayerControlsMoveDownActionCancelled;
+        private InputAction m_PlayerControls_MoveLeft;
+        [SerializeField] private ActionEvent m_PlayerControlsMoveLeftActionStarted;
+        [SerializeField] private ActionEvent m_PlayerControlsMoveLeftActionPerformed;
+        [SerializeField] private ActionEvent m_PlayerControlsMoveLeftActionCancelled;
+        private InputAction m_PlayerControls_MoveRight;
+        [SerializeField] private ActionEvent m_PlayerControlsMoveRightActionStarted;
+        [SerializeField] private ActionEvent m_PlayerControlsMoveRightActionPerformed;
+        [SerializeField] private ActionEvent m_PlayerControlsMoveRightActionCancelled;
         public struct PlayerControlsActions
         {
             private PlayerInput m_Wrapper;
@@ -75,6 +129,18 @@ namespace Assets.Scripts.Player
             public ActionEvent MoveUpStarted { get { return m_Wrapper.m_PlayerControlsMoveUpActionStarted; } }
             public ActionEvent MoveUpPerformed { get { return m_Wrapper.m_PlayerControlsMoveUpActionPerformed; } }
             public ActionEvent MoveUpCancelled { get { return m_Wrapper.m_PlayerControlsMoveUpActionCancelled; } }
+            public InputAction @MoveDown { get { return m_Wrapper.m_PlayerControls_MoveDown; } }
+            public ActionEvent MoveDownStarted { get { return m_Wrapper.m_PlayerControlsMoveDownActionStarted; } }
+            public ActionEvent MoveDownPerformed { get { return m_Wrapper.m_PlayerControlsMoveDownActionPerformed; } }
+            public ActionEvent MoveDownCancelled { get { return m_Wrapper.m_PlayerControlsMoveDownActionCancelled; } }
+            public InputAction @MoveLeft { get { return m_Wrapper.m_PlayerControls_MoveLeft; } }
+            public ActionEvent MoveLeftStarted { get { return m_Wrapper.m_PlayerControlsMoveLeftActionStarted; } }
+            public ActionEvent MoveLeftPerformed { get { return m_Wrapper.m_PlayerControlsMoveLeftActionPerformed; } }
+            public ActionEvent MoveLeftCancelled { get { return m_Wrapper.m_PlayerControlsMoveLeftActionCancelled; } }
+            public InputAction @MoveRight { get { return m_Wrapper.m_PlayerControls_MoveRight; } }
+            public ActionEvent MoveRightStarted { get { return m_Wrapper.m_PlayerControlsMoveRightActionStarted; } }
+            public ActionEvent MoveRightPerformed { get { return m_Wrapper.m_PlayerControlsMoveRightActionPerformed; } }
+            public ActionEvent MoveRightCancelled { get { return m_Wrapper.m_PlayerControlsMoveRightActionCancelled; } }
             public InputActionMap Get() { return m_Wrapper.m_PlayerControls; }
             public void Enable() { Get().Enable(); }
             public void Disable() { Get().Disable(); }
@@ -88,6 +154,15 @@ namespace Assets.Scripts.Player
                     MoveUp.started -= m_Wrapper.m_PlayerControlsActionsCallbackInterface.OnMoveUp;
                     MoveUp.performed -= m_Wrapper.m_PlayerControlsActionsCallbackInterface.OnMoveUp;
                     MoveUp.cancelled -= m_Wrapper.m_PlayerControlsActionsCallbackInterface.OnMoveUp;
+                    MoveDown.started -= m_Wrapper.m_PlayerControlsActionsCallbackInterface.OnMoveDown;
+                    MoveDown.performed -= m_Wrapper.m_PlayerControlsActionsCallbackInterface.OnMoveDown;
+                    MoveDown.cancelled -= m_Wrapper.m_PlayerControlsActionsCallbackInterface.OnMoveDown;
+                    MoveLeft.started -= m_Wrapper.m_PlayerControlsActionsCallbackInterface.OnMoveLeft;
+                    MoveLeft.performed -= m_Wrapper.m_PlayerControlsActionsCallbackInterface.OnMoveLeft;
+                    MoveLeft.cancelled -= m_Wrapper.m_PlayerControlsActionsCallbackInterface.OnMoveLeft;
+                    MoveRight.started -= m_Wrapper.m_PlayerControlsActionsCallbackInterface.OnMoveRight;
+                    MoveRight.performed -= m_Wrapper.m_PlayerControlsActionsCallbackInterface.OnMoveRight;
+                    MoveRight.cancelled -= m_Wrapper.m_PlayerControlsActionsCallbackInterface.OnMoveRight;
                 }
                 m_Wrapper.m_PlayerControlsActionsCallbackInterface = instance;
                 if (instance != null)
@@ -95,6 +170,15 @@ namespace Assets.Scripts.Player
                     MoveUp.started += instance.OnMoveUp;
                     MoveUp.performed += instance.OnMoveUp;
                     MoveUp.cancelled += instance.OnMoveUp;
+                    MoveDown.started += instance.OnMoveDown;
+                    MoveDown.performed += instance.OnMoveDown;
+                    MoveDown.cancelled += instance.OnMoveDown;
+                    MoveLeft.started += instance.OnMoveLeft;
+                    MoveLeft.performed += instance.OnMoveLeft;
+                    MoveLeft.cancelled += instance.OnMoveLeft;
+                    MoveRight.started += instance.OnMoveRight;
+                    MoveRight.performed += instance.OnMoveRight;
+                    MoveRight.cancelled += instance.OnMoveRight;
                 }
             }
         }
@@ -114,5 +198,8 @@ namespace Assets.Scripts.Player
     public interface IPlayerControlsActions
     {
         void OnMoveUp(InputAction.CallbackContext context);
+        void OnMoveDown(InputAction.CallbackContext context);
+        void OnMoveLeft(InputAction.CallbackContext context);
+        void OnMoveRight(InputAction.CallbackContext context);
     }
 }
