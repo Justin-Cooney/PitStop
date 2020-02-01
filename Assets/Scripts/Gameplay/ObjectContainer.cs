408bc28e4bf439f4c4aae78ae94139b8d1c8c36c
Kent-Desktop\Kent <theswordbattler@gmail.com>
2020-02-01 12:36:54 -0600

    Getting things in ship shape.

---
+++ Assets/Scripts/Gameplay/ObjectContainer.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ObjectContainer : ScriptableObject {
+
+    public ObjectType type;
+    public List<ObjectProperty> properties;
+
+}
