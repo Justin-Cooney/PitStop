73640d3faf41eef562b532f4ded6fc871d6d175f
Justin-Cooney <justin.d.cooney@gmail.com>
2020-02-01 21:17:13 -0600

    better floor and lights

--- Assets/Scripts/Lighting/FlashingLight.cs
+++ Assets/Scripts/Lighting/FlashingLight.cs
@@ -1,11 +1,27 @@
-﻿using System.Collections;
+﻿using Assets.Scripts.Events;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Zenject;
+using UniRx;
 
 public class FlashingLight : MonoBehaviour
 {
     private Light _light;
     private bool _increasing;
+    [Inject]
+    public IObservable<ShakeCamera> _shakeCamera;
+
+    public float _activeTime = 0;
+
+    [Inject]
+    public void Initialize()
+    {
+        _shakeCamera.Subscribe(e => {
+            _activeTime = 5;
+        });
+    }
 
     void Start()
     {
@@ -15,6 +31,10 @@ public class FlashingLight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        _activeTime -= 1 * Time.deltaTime;
+        if (_activeTime <= 0)
+            _increasing = false;
+
         if (_increasing)
             _light.intensity += 1 * Time.deltaTime;
         else
